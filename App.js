b626b3154a91e3d53e39f6724d8b34ae488a0146
Sandeep Kumar <sandeep@code-brew.com>
2019-12-12 19:12:54 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,147 @@
+import React, { Component } from 'react';
+//Import React
+import { Platform, StyleSheet, Text, View } from 'react-native';
+//Import Basic React Native Component
+import Video from 'react-native-video';
+//Import React Native Video to play video
+import MediaControls, { PLAYER_STATES } from 'react-native-media-controls';
+//Media Controls to control Play/Pause/Seek and full screen
+import YouTube from 'react-native-youtube';
+class App extends Component {
+  videoPlayer;
+ 
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentTime: 0,
+      duration: 0,
+      isFullScreen: false,
+      isLoading: true,
+      paused: false,
+      playerState: PLAYER_STATES.PLAYING,
+      screenType: 'content',
+    };
+  }
+ 
+  onSeek = seek => {
+    //Handler for change in seekbar
+    this.videoPlayer.seek(seek);
+  };
+ 
+  onPaused = playerState => {
+    //Handler for Video Pause
+    this.setState({
+      paused: !this.state.paused,
+      playerState,
+    });
+  };
+ 
+  onReplay = () => {
+    //Handler for Replay
+    this.setState({ playerState: PLAYER_STATES.PLAYING });
+    this.videoPlayer.seek(0);
+  };
+ 
+  onProgress = data => {
+    const { isLoading, playerState } = this.state;
+    // Video Player will continue progress even if the video already ended
+    if (!isLoading && playerState !== PLAYER_STATES.ENDED) {
+      this.setState({ currentTime: data.currentTime });
+    }
+  };
+  
+  onLoad = data => this.setState({ duration: data.duration, isLoading: false });
+  
+  onLoadStart = data => this.setState({ isLoading: true });
+  
+  onEnd = () => this.setState({ playerState: PLAYER_STATES.ENDED });
+  
+  onError = () => alert('Oh! ', error);
+  
+  exitFullScreen = () => {
+    alert('Exit full screen');
+  };
+  
+  enterFullScreen = () => {};
+  
+  onFullScreen = () => {
+    if (this.state.screenType == 'content')
+      this.setState({ screenType: 'cover' });
+    else this.setState({ screenType: 'content' });
+  };
+  renderToolbar = () => (
+    <View>
+      <Text> toolbar </Text>
+    </View>
+  );
+  onSeeking = currentTime => this.setState({ currentTime });
+ 
+  render() {
+    return (
+      <View style={styles.container}>
+        {/* <Video
+          onEnd={this.onEnd}
+          onLoad={this.onLoad}
+          onLoadStart={this.onLoadStart}
+          onProgress={this.onProgress}
+          paused={this.state.paused}
+          ref={videoPlayer => (this.videoPlayer = videoPlayer)}
+          resizeMode={this.state.screenType}
+          onFullScreen={this.state.isFullScreen}
+          source={{ uri: 'https://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4' }}
+          style={styles.mediaPlayer}
+          volume={10}
+        />
+        <MediaControls
+          duration={this.state.duration}
+          isLoading={this.state.isLoading}
+          style={{height : 50}}
+          onFullScreen={this.onFullScreen}
+          onPaused={this.onPaused}
+          onReplay={this.onReplay}
+          onSeek={this.onSeek}
+          onSeeking={this.onSeeking}
+          playerState={this.state.playerState}
+          progress={this.state.currentTime}
+          isFullScreen = {false}
+        /> */}
+       <YouTube
+        apiKey="AIzaSyAeQHp0Ek7DIJTu0_zbZ0h3r1GqpDR8Ooo"
+  videoId="PBCYotk_SCY" // The YouTube video ID
+  play // control playback of video with true/false
+  
+  loop // control whether the video should loop when ended
+  onReady={e => this.setState({ isReady: true })}
+  onChangeState={e => this.setState({ status: e.state })}
+  onChangeQuality={e => this.setState({ quality: e.quality })}
+  onError={e => this.setState({ error: e.error })}
+  style={{ alignSelf: 'stretch', height: 300, marginTop :20 }}
+/>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems : "center",
+    marginHorizontal : 24
+  },
+  toolbar: {
+    marginTop: 30,
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 5,
+  },
+  mediaPlayer: {
+    marginTop: 180,
+    width : '100%',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    alignSelf : "center"
+  },
+});
+export default App;
